84c5c19665f3954f0082cab3bd435a2c4340e34f
Vlad Donskoy <vdonskoy3@gmail.com>
2014-07-17 17:46:24 -0400

    Adds working Perform_Moves method

--- checkers.rb
+++ checkers.rb
@@ -1,4 +1,3 @@
-require 'debugger'
 class Board
   attr_accessor :grid
 
@@ -7,12 +6,10 @@ class Board
   end
 
   def [](pos)
-      #raise RuntimeError.new("Please enter a valid position.") if pos.any? {|el| el.nil?}
       self.grid[pos[0]][pos[1]]
     end
 
     def []=(pos, piece)
-      #raise RuntimeError.new("Please enter a valid position.") if pos.any? {|el| el.nil?}
       self.grid[pos[0]][pos[1]] = piece
     end
 
@@ -51,18 +48,68 @@ class Board
     false
   end
 
+  def checkmate?(color)
+    self.grid.each do |row|
+      row.each do |space|
+        unless space.nil?
+          if space.color == color
+            return false
+          end
+        end
+      end
+      true
+    end
+  end
+
   def render
-    (0..7).each do |row|
-      (0..7).each do |column|
-        pos = [row,column]
-        if self[pos] == nil && row % 2 != column % 2
+    column_labels = {
+      1 => "a",
+      2 => "b",
+      3 => "c",
+      4 => "d",
+      5 => "e",
+      6 => "f",
+      7 => "g",
+      8 => "h"
+    }
+
+    row_labels = {
+      1 => "8",
+      2 => "7",
+      3 => "6",
+      4 => "5",
+      5 => "4",
+      6 => "3",
+      7 => "2",
+      8 => "1"
+    }
+    (0..9).each do |row|
+      (0..9).each do |column|
+        pos = [row-1,column-1]
+        if row == 0 || row == 9
+          if column_labels[column]
+            print "#{column_labels[column]}"
+          else
+            print " "
+          end
+        elsif column == 0 || column == 9
+          if row_labels[row]
+            print "#{row_labels[row]}"
+          else
+            print " "
+          end
+        elsif self[pos] == nil && (row-1) % 2 != (column-1) % 2
           print "■"
         elsif self[pos] == nil
           print "□"
-        elsif self[pos].color == "white"
+        elsif self[pos].color == "white" && self[pos].is_king == false
           print "☺"
-        elsif self[pos].color == "black"
+        elsif self[pos].color == "black" && self[pos].is_king == false
           print "☻"
+        elsif self[pos].color == "white" && self[pos].is_king == true
+          print "♔"
+        elsif self[pos].color == "black" && self[pos].is_king == true
+          print "♚"
         end
       end
       puts
@@ -71,15 +118,16 @@ class Board
 end
 
 class Piece
-  attr_accessor :pos, :color, :board
+  attr_accessor :pos, :color, :board, :is_king
 
   def initialize(pos,color,board)
     @pos = pos
     @color = color
     @board = board
+    @is_king = false
   end
 
-  def moves
+  def slide_moves
     possible_moves = []
     self.move_diffs.each do |delta|
       coords = @pos.dup
@@ -87,7 +135,18 @@ class Piece
       coords[1] += delta[1]
       if ((0..7).to_a.include?(coords[0]) && (0..7).to_a.include?(coords[1])) && @board[coords].nil?
         possible_moves << coords
-      elsif @board.contains_enemy_piece?(self.pos,coords) && !@board.contains_enemy_piece?(self.pos,[coords[0]+delta[0],coords[1]+delta[1]])
+      end
+    end
+    possible_moves
+  end
+
+  def jump_moves
+    possible_moves = []
+    self.move_diffs.each do |delta|
+      coords = @pos.dup
+      coords[0] += delta[0]
+      coords[1] += delta[1]
+      if @board.contains_enemy_piece?(self.pos,coords) && !@board.contains_enemy_piece?(self.pos,[coords[0]+delta[0],coords[1]+delta[1]])
         possible_moves << [coords[0]+delta[0],coords[1]+delta[1]]
       end
     end
@@ -95,7 +154,9 @@ class Piece
   end
 
   def move_diffs
-    if self.color == "white"
+    if @is_king
+      deltas = [[1,-1],[1,1],[-1,-1],[-1,1]]
+    elsif self.color == "white"
       deltas = [[1,-1],[1,1]]
     elsif self.color == "black"
       deltas =[[-1,-1],[-1,1]]
@@ -104,39 +165,95 @@ class Piece
   end
 
   def perform_slide(pos, target_coords)
-    if self.moves.include?(target_coords)
-      self.board[target_coords] = self.board[pos].class.new(target_coords, self.board[pos].color, self.board)
+    #if self.slide_moves.include?(target_coords)
+      self.board[target_coords] = self.board[pos].class.new(target_coords.dup, self.board[pos].color, self.board)
       self.board[pos] = nil
-      #self[target_coords].pos = target_coords.dup
+      self.maybe_promote
+      #end
+  end
+
+  def perform_jump(pos, target_coords)
+    #if self.jump_moves.include?(target_coords)
+      self.board[target_coords] = self.board[pos].class.new(target_coords.dup, self.board[pos].color, self.board)
+      self.board[pos] = nil
+      self.delete_opponent(target_coords)
+      self.maybe_promote
+      #end
+  end
+
+  def perform_moves!(start_pos,end_pos)
+    if self.slide_moves.include?(end_pos)
+      perform_slide(start_pos,end_pos)
+    elsif self.jump_moves.include?(end_pos)
+      perform_jump(start_pos,end_pos)
     end
   end
 
-  def perform_jump
-
+  def delete_opponent(target_coords)
+    opponent_coords = ((pos[0]+target_coords[0])/2),((pos[1]+target_coords[1])/2)
+    self.board[opponent_coords] = nil
   end
 
   def maybe_promote
-
-  end
-
-  def promote
-
+    if self.pos[0] == 0 && self.color == "black"
+      @is_king = true
+    elsif self.pos[0] == 7 && self.color == "white"
+      @is_king = true
+    end
   end
 end
 
 class Game
   def initialize
-    b = Board.new
-    b.board_fill
-    b.render
-    pos = [2,1]
-    pos2 = [3,0]
-    b[pos].perform_slide(pos, pos2)
-    b.render
+    @board = Board.new
+    @board.board_fill
+    self.play
   end
 
   def play
+    turn = 1
+    conversions = {
+      "a" => 0,
+      "b" => 1,
+      "c" => 2,
+      "d" => 3,
+      "e" => 4,
+      "f" => 5,
+      "g" => 6,
+      "h" => 7,
+      "8" => 0,
+      "7" => 1,
+      "6" => 2,
+      "5" => 3,
+      "4" => 4,
+      "3" => 5,
+      "2" => 6,
+      "1" => 7,
+    }
+    turn_colors = ["black","white"]
+    turn_color = turn_colors[0]
+    until @board.checkmate?(turn_color)
+      @board.render
+      turn_color = turn_colors[turn % 2]
+      puts "#{turn_color.capitalize} turn"
+      puts "Enter the coordinates of the piece you would like to move:"
+      start_pos = gets.chomp.split("").reverse.map { |num| conversions[num] }
+      if !@board[start_pos].nil? && @board[start_pos].color != turn_color
+        puts "That is not your piece!"
+        next
+      end
+      run = "y"
+      until run == "n"
+        puts "Enter the coordinates of the space you would like to move to:"
+        end_pos = gets.chomp.split("").reverse.map { |num| conversions[num] }
+        @board[start_pos].perform_moves!(start_pos,end_pos)
+        puts "Would you like to move again?"
+        run = gets.chomp
+        start_pos = end_pos
+      end
+      turn += 1
 
+    end
   end
 end
 
