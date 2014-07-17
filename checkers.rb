45cdca6b7c5cb148bf22573789cdc6b8418ab399
Vlad Donskoy <vdonskoy3@gmail.com>
2014-07-17 11:03:27 -0400

    Working Board_Fill and Render methods

--- checkers.rb
+++ checkers.rb
@@ -5,37 +5,49 @@ class Board
     @grid = Array.new(8) {Array.new(8)}
   end
 
+  def [](pos)
+      #raise RuntimeError.new("Please enter a valid position.") if pos.any? {|el| el.nil?}
+      self.grid[pos[0]][pos[1]]
+    end
+
+    def []=(pos, piece)
+      #raise RuntimeError.new("Please enter a valid position.") if pos.any? {|el| el.nil?}
+      self.grid[pos[0]][pos[1]] = piece
+    end
+
   def board_fill
-    self.grid.each do |row|
-      self.grid.each do |space|
-        if row.index == 0 && space.index % 2 == 1
-          space = Piece.new([row,space],"white")
-        elsif row.index == 1 && space.index % 2 == 0
-          space = Piece.new([row,space],"white")
-        elsif row.index == 2 && space.index % 2 == 1
-          space = Piece.new([row,space],"white")
-        elsif row.index == 5 && space.index % 2 == 0
-          space = Piece.new([row,space],"black")
-        elsif row.index == 6 && space.index % 2 == 1
-          space = Piece.new([row,space],"black")
-        elsif row.index == 7 && space.index % 2 == 0
-          space = Piece.new([row,space],"black")
+    (0..7).each do |row|
+      (0..7).each do |column|
+        pos = [row,column]
+        if row == 0 && column % 2 == 1
+          self[pos] = Piece.new(pos,"white",self)
+        elsif row == 1 && column % 2 == 0
+          self[pos] = Piece.new(pos,"white",self)
+        elsif row == 2 && column % 2 == 1
+          self[pos] = Piece.new(pos,"white",self)
+        elsif row == 5 && column % 2 == 0
+          self[pos] = Piece.new(pos,"black",self)
+        elsif row == 6 && column % 2 == 1
+          self[pos] = Piece.new(pos,"black",self)
+        elsif row == 7 && column % 2 == 0
+          self[pos] = Piece.new(pos,"black",self)
         end
       end
     end
   end
 
   def render
-    self.grid.each do |row|
-      self.grid.each do |space|
-        if space == nil && (row.index % 2 == 0 && space.index % 2 == 0) || (row.index % 2 == 1 && space.index % 2 == 1)
-          print □
-        elsif space == nil && (row.index % 2 == 0 && space.index % 2 == 1) || (row.index % 2 == 1 && space.index % 2 == 0)
-          print ■
-        elsif space.color == "white"
-          print ☺
-        elsif space.color == "black"
-          print ☻
+    (0..7).each do |row|
+      (0..7).each do |column|
+        pos = [row,column]
+        if self[pos] == nil && row % 2 != column % 2
+          print "■"
+        elsif self[pos] == nil
+          print "□"
+        elsif self[pos].color == "white"
+          print "☺"
+        elsif self[pos].color == "black"
+          print "☻"
         end
       end
       puts
@@ -43,5 +55,30 @@ class Board
   end
 end
 
+class Piece
+  attr_accessor :pos, :color, :board
+
+  def initialize(pos,color,board)
+    @pos = pos
+    @color = color
+    @board = board
+  end
+
+  def moves(piece)
+    possible_moves = []
+
+  end
+
+  def move_diffs(piece)
+    if piece.color == "white"
+      deltas = [[-1,1],[1,1]]
+    elsif piece.color == "black"
+      deltas =[[-1,-1],[1,-1]]
+    end
+    deltas
+  end
+end
+
 b = Board.new
+b.board_fill
 b.render
